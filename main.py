cd6994cddfe681a75d9e197f405e494790a00267
Luker <luker.plus@gmail.com>
2019-01-03 14:54:43 +0100

    82/91

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pylab import *
-from numpy import *
+import numpy as np
 from scipy import *
 import scipy.io.wavfile
 from scipy import signal
@@ -11,8 +11,9 @@ fig = plt.figure(figsize=(15, 15), dpi=80)
 
 
 def sprawdzacz(signalData,w):
-    low=150
+    low=120
     high=6000
+    signalData=np.array(signalData)
     if (len(signalData.shape) > 1):
         signalData = [s[0] for s in signalData]
 
@@ -32,14 +33,16 @@ def sprawdzacz(signalData,w):
     for i in freqs:
         signal.append(signalfft[i])
 
+    for i in range(0, int(len(signalfft)/2)):
+        if i<low or i>high:
+            signal[i]=0
     output = []
     wynik = signal.copy()
     output.append(signal)
     for i in range(1, 8):
         output.append(scipy.signal.decimate(signal, i))
         for j in range(len(output[i])):
-            if j>low and j<high:
-                wynik[j] = wynik[j] * output[i][j]
+            wynik[j] = wynik[j] * output[i][j]
 
     for i in range(len(wynik)):
         if wynik[i] < 1:
@@ -52,8 +55,8 @@ def sprawdzacz(signalData,w):
     #ax.stem(freqs, signal, '-')
     #plt.yscale('log')
     #plt.show()
-    print(freqs[argmax(wynik)])
-    if freqs[argmax(wynik)] > 350:
+    print(freqs[argmax(wynik,0)])
+    if freqs[argmax(wynik,0)] > 350:
         return("K")
     else:
         return("M")
@@ -68,16 +71,19 @@ def checkall():
 
     wygranko = 0
     przegranko = 0
-    for i in range(1, len(listaPlikow)):
-        w, signalData = scipy.io.wavfile.read(listaPlikow[i])
-        wynik=sprawdzacz(signalData, w)
+    for i in range(0, len(listaPlikow)):
+        try:
+            w, signalData = scipy.io.wavfile.read(listaPlikow[i])
+            wynik=sprawdzacz(signalData, w)
+        except:
+            wynik='K'
         if wynik == listaPoprawnych[i]:
             wygranko=wygranko+1
         else:
             przegranko=przegranko+1
-        print(wynik, listaPoprawnych[i])
+        print(listaPlikow[i], wynik)
     print("wygranka ",wygranko, "przegranka", przegranko)
 
-#w, signalData = scipy.io.wavfile.read("trainall/010_M.wav")
-#sprawdzacz(signalData, w);
+#w, signalData = scipy.io.wavfile.read("trainall/067_K.wav")
+#sprawdzacz(signalData, w)
 checkall()
